18e422fe74676cd153c5d5d7cef8169b23aaa351
jae9380 <ljy5314@gmail.com>
2024-07-08 13:24:54 +0900

    create project

---
+++ src/main/java/com/ll/fillgo/FillGoApplication.java
@@ -0,0 +1,13 @@
+package com.ll.fillgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FillGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FillGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/fillgo/FillGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.fillgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FillGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
